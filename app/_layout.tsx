9ea72a3c4e6967228f2f314ce7c5e67c51d0723d
rahelhueppi <r.hueppi2008@gmail.com>
2026-03-25 11:23:07 +0100

    implement context

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,33 @@
+import { SkillProvider } from "@/context/skillContext";
 import { Stack } from "expo-router";
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <SkillProvider>
+      <Stack
+        screenOptions={{
+          headerStyle: {
+            backgroundColor: "#ac95a8",
+          },
+          headerTintColor: "#fff",
+          headerTitleStyle: {
+            fontWeight: "bold",
+          },
+        }}
+      >
+        <Stack.Screen
+          name="index"
+          options={{
+            title: "Meine Skills",
+          }}
+        />
+        <Stack.Screen
+          name="skillDetailScreen"
+          options={{
+            title: "Details",
+          }}
+        />
+      </Stack>
+    </SkillProvider>
+  );
 }
--- app/index.tsx
+++ app/index.tsx
@@ -1,82 +1,19 @@
-import { FlatList, StyleSheet, Text, View } from "react-native";
+import { useSkill } from "@/context/skillContext";
+import { FlatList, StyleSheet, View } from "react-native";
 import SkillItem from "../components/SkillItem";
-import Skill from "../models/skill";
 
 export default function Index() {
+  const { skillList } = useSkill();
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Skill Tracker</Text>
-
       <FlatList
-        data={testSkills}
+        data={skillList}
         keyExtractor={(item) => item.name}
         renderItem={({ item }) => <SkillItem skill={item} />}
       />
     </View>
   );
 }
-
-const testSkills: Skill[] = [
-  {
-    name: "Développé à la seconde",
-    goal: 135,
-    entries: [
-      {
-        id: "1",
-        date: "2026-03-20",
-        photoUri: "file://photo1.jpg",
-        value: 110,
-        note: "noch nicht ganz stabil",
-      },
-      {
-        id: "2",
-        date: "2026-03-22",
-        photoUri: "file://photo2.jpg",
-        value: 120,
-        note: "besserer turnout",
-      },
-    ],
-  },
-  {
-    name: "Oversplit",
-    goal: 10,
-    entries: [
-      {
-        id: "3",
-        date: "2026-03-21",
-        photoUri:
-          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTxWZL7-RjhrSQVB9RS-PbGbCyzfWkYVEKI1A&s",
-        value: 5,
-        note: "mit Blöcken",
-      },
-      {
-        id: "4",
-        date: "2026-03-24",
-        photoUri:
-          "https://i.redd.it/working-on-oversplits-and-wanted-to-share-my-progress-the-v0-nw4zsi7y488a1.jpg?width=3468&format=pjpg&auto=webp&s=057e78785a04ce3a50e404ea14c41776531cc943",
-        value: 10,
-        note: "Ziel erreicht",
-      },
-    ],
-  },
-  {
-    name: "Choreo Giselle Variation",
-    entries: [
-      {
-        id: "5",
-        date: "2026-03-18",
-        photoUri: "file://photo5.jpg",
-        note: "erste Durchläufe, unsicher",
-      },
-      {
-        id: "6",
-        date: "2026-03-25",
-        photoUri: "file://photo6.jpg",
-        note: "viel sauberer und sicherer",
-      },
-    ],
-  },
-];
 
 const styles = StyleSheet.create({
   container: {
